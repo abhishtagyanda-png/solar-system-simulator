94b5ef5117c202167e77d2e1f9481ca9b8293d24
Abhishta Gyanda <abhishtagyanda@gmail.com>
2026-06-19 02:56:05 +0530

    Add sun using turtle graphics

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("Solar System Simulator")
+
+import turtle
+
+screen = turtle.Screen()
+screen.title("Solar System Simulator")
+
+sun = turtle.Turtle()
+sun.shape("circle")
+sun.color("yellow")
+sun.penup()
+
+screen.mainloop()
